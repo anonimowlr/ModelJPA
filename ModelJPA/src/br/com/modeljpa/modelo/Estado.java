4cfe59c74ce6986d14de8687806a95e565ff2f60
PC_LENOVO <PC_LENOVO@192.168.1.101>
2019-01-13 20:39:03 -0200

    Junit implementado

--- ModelJPA/src/br/com/modeljpa/modelo/Estado.java
+++ ModelJPA/src/br/com/modeljpa/modelo/Estado.java
@@ -1,8 +1,18 @@
 package br.com.modeljpa.modelo;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 
 @Entity
@@ -10,11 +20,32 @@ public class Estado implements Serializable {
     
     
     @Id
+    @SequenceGenerator(name = "seq_estado", sequenceName = "seq_estado_id", allocationSize = 1)
+    @GeneratedValue(generator = "seq_estado",strategy = GenerationType.SEQUENCE)
     private Integer id;
+    @Column(name = "nome" ,length = 50, nullable = false)
+    @NotBlank(message = "O nome deve ser informado")
+    @NotNull(message = "O nome nao pode ser nulo  ")
+    @Length(max = 50,message = "O nome nao pode ter mais de {max} caracteres")
     private String  nome;
+     @Column(name = "uf " ,length = 50, nullable = false)
+    @NotBlank(message = "O uf deve ser informada")
+    @NotNull(message = "A  uf nao pode ser nuloa ")
+    @Length(max = 2,message = "A uf nao pode ter mais de {max} caracteres")
     private String uf;
+     
+     @NotNull(message = "O pais deve ser informado")
+     @ManyToOne
+     @JoinColumn(name = "pais_id",referencedColumnName = "id",nullable = false)
+   
     private Pais pais;
 
+    public Estado() {
+    }
+
+     
+     
+     
     /**
      * @return the id
      */
---
+++ ModelJPA/test/br/com/modeljpa/junit/TestePersistirEstado.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modeljpa.junit;
+
+import br.com.modeljpa.jpa.EntityManagerUtil;
+import br.com.modeljpa.modelo.Estado;
+import br.com.modeljpa.modelo.Pais;
+import javax.persistence.EntityManager;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author PC_LENOVO
+ */
+public class TestePersistirEstado {
+    
+    EntityManager em;
+    
+    
+    public TestePersistirEstado() {
+    }
+    
+    @Before
+    public void setUp() {
+        em = EntityManagerUtil.getEntityManager();
+    }
+    
+    @After
+    public void tearDown() {
+        em.close();
+    }
+    
+
+   
+    @Test
+    public void teste(){
+        boolean exception = false;
+        
+        try{
+            Estado e = new Estado();
+            e.setNome("Rio grande do sul");
+            e.setUf("Rs");
+            e.setPais(em.find(Pais.class, 2));
+            
+            
+            em.getTransaction().begin();
+            em.persist(e);
+            em.getTransaction().commit();
+        }catch(Exception e){
+            exception=true;
+            e.printStackTrace();
+        }
+        
+        Assert.assertEquals(false, false);
+        
+    }
+    
+    
+}
